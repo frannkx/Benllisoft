ccd954311013871d01c08d5833c615e77aa39e77
frannkx <frannkx@gmail.com>
2017-05-10 14:16:48 -0400

    Add files via upload
    Carga inicial de los archivos del curso de Programacion Estructurada

---
+++ P.E/matriz.js
@@ -0,0 +1,9 @@
+/*alert("Matrices");
+
+var menu=["Productos","Ventas","Contacto"];
+document.write(menu[1]);
+
+*/
+
+var dofa=[["Fortalezas","Oportunidades"],["Debilidades","Amenazas"]];
+document.write(dofa[0][2]);
