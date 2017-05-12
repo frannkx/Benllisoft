03dbae301d1bc79a144ef1c04faf44b872e2c102
LAPTOP-DELL\Francisco <frannkx@gmail.com>
2017-05-11 21:31:32 -0400

    Creacion de la carpeta P.E/js

---
+++ P.E/js/script.js
@@ -0,0 +1,24 @@
+/* Parte 1
+var nombre="Francisco";
+var apellido="Paredes";
+var edad="27";
+alert(nombre+" "+apellido+" \n"+edad+" años.");
+
+*/
+
+/* Parte 2
+
+var peso=70;
+peso=prompt("Cual es tu peso");
+alert("Tu peso es"+" "+peso);
+
+
+//Parte 3
+
+var pesoEnMarte;
+
+alert("Cual es tu peso en marte")
+peso=prompt("Dime tu peso en Kg");
+peso=parseInt(peso);
+pesoEnMarte= (peso / 9.81)* 3.711;
+alert("Tu peso en marte es "+pesoEnMarte);
