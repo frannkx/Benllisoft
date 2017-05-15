685beb4b7aa8cf8076fa9c7e9ca0e30502ae8733
Francisco Paredes <frannkx@gmail.com>
2017-05-15 13:03:46 -0400

    Finalización del ejercicio de campo minado

--- P.E/js/matriz.js
+++ P.E/js/matriz.js
@@ -9,15 +9,17 @@ var dofa=[["Fortalezas","Oportunidades"],["Debilidades","Amenazas"]];
 document.write(dofa[0][2]);
 */
 
+//Funciones
 function explosion(){
      alert("!!!BOOM!!!");
      document.write("<h1>!!!BOOM!!! Elegiste un area minada</h1>")
 }
 
 function ganaste(){
-     document.write("Eres un ganador!!!");
+     document.write("<h1>Eres un ganador!!!</h1>");
 }
 
+//Declaracion de variables
 
 var campo = [  [1,0,0],
                [0,1,0],
@@ -25,18 +27,22 @@ var campo = [  [1,0,0],
 var x,y;
 var textos=["Cesped ","Bomba "];
 
+
+// Inicio del programa
+
 alert("Estas en un campo minado\n"+"Elije una posición entre el 0 y el 2 para el eje X y para el eje Y");
 x=prompt("Posición en x? (entre 0 y 2)");
 y=prompt("Posición en y? (entre 0 y 2)");
 
 
-var posicion;
-posicion = campo[x][y];
-document.write(textos[posicion]);
-
-
 if(x<3 && y<3){
-     document.write("sitio correcto");
+     var posicion = campo[x][y];
+     document.write("Elegiste "+textos[posicion]);
+     if (posicion==1){
+          explosion();
+     } else {
+          ganaste();
+     }
 }else{
      document.write("te saliste del campo");
      explosion();
