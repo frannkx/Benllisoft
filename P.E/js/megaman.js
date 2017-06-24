5fb727f69af50af8685346bc93fbc8e4606717ac
LAPTOP-DELL\Francisco <frannkx@gmail.com>
2017-06-24 00:31:01 -0400

    Dibujado del fondo

--- P.E/js/megaman.js
+++ P.E/js/megaman.js
@@ -1,1 +1,15 @@
 var tablero
+var fondo={
+     imagenURL:"img/fondo.png"
+};
+function inicio(){
+     var canvas=document.getElementById("campo");
+     tablero=canvas.getContext("2d");
+     fondo.imagen= new Image();
+     fondo.imagen.src=fondo.imagenURL;
+     fondo.imagen.onload=dibujarFondo;
+}
+
+function dibujarFondo(){
+     tablero.drawImage(fondo.imagen, 0, 0);
+}
